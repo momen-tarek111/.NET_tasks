29a3fef42dd858cb0c29218872d8be549cf333e2
momen-tarek111 <110129557+momen-tarek111@users.noreply.github.com>
2024-02-09 22:17:32 +0200

    Add files via upload

---
+++ session4/Models/Models/Category.cs
@@ -0,0 +1,12 @@
+﻿namespace project2.Models
+{
+    public class Category
+    {
+        public Category() {
+            this.Id = 0;
+            
+        }
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ session4/Models/Models/Product.cs
@@ -0,0 +1,13 @@
+﻿namespace project2.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int Quantity { get; set; }
+        public float Price { get; set; }
+        public bool EnableSize { get; set; }
+        public Company company { get; set; }
+    }
+}
